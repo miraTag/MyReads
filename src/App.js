60b0d19e445a7178ba3f8eb94fdea6736dae345d
Mira Ebrahim <jsmirajs@gmail.com>
2022-11-09 08:55:49 +0000

    SEARCH

--- src/App.js
+++ src/App.js
@@ -1,100 +1,43 @@
-import React, { Component } from 'react'
-import { Route } from 'react-router-dom'
-import * as BooksAPI from './BooksAPI'
-import ListBooks from './components/ListBooks'
-import SearchBook from './components/SearchBook'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import * as BooksAPI from "./BooksAPI";
+import Listbooks from "./components/ListBooks";
+import SearchBook from "./components/SearchBook";
+import "./App.css";
+const arrShelf = ["currentlyReading", "read", "wantToRead"];
+const App = () => {
+  const [books, setBooks] = useState([]);
+  useEffect(() => {
+    const getBooks = async () => {
+      const res = await BooksAPI.getAll();
+      setBooks(res);
+    };
+    getBooks();
+  }, []);
 
-
-class App extends Component {
-  state = {
-    books: [],
-    showSearchPage: false,
-    resultBooks: []
-  }
-    componentDidMount() {
-    BooksAPI.getAll().then((books) => {
-     this.setState({ books })
-    })
-  }
-
-  changeShelf(book, shelf){
-
-    // See if the book is in the list
-
-    const index = this.state.books.findIndex((key) => {
-      return key.id === book.id;
-    });
-    let tempList = this.state.books;
-
-    if (index === -1) {
-      // the book is not in the list, copy it
-      book.shelf = shelf;
-      tempList.push(book);
-
-    } else {
-
-      // it is already in the list just change the shelf
-      tempList[index].shelf = shelf;
-    }
-
-    BooksAPI.update(book, shelf).then(
-      this.setState({ books: tempList })
-    );
-
-
-
-}; 
-
-setQuery = (query) => {
-  if(query){
-      BooksAPI.search(query).then((books) => {
-          // if the BookAPI.search worked properly, this would be unnecessary
-          if(books.length){
-              books.forEach((book, index) => {
-                  let myBook = this.state.books.find((b) => b.id === book.id);
-                  book.shelf = myBook ? myBook.shelf : 'none';
-                  books[index] = book;
-              });
-
-              this.setState({
-                  resultBooks: books
-              });
-          }
-
-      });
-      } else {
-      this.setState({
-          resultBooks: []
-      });
-  }
-};
-
-getBookshelf(book){
-  this.props.books.filter((book) => book.shelf)
-};
- 
-
-
-  render() {
-    return (
-      <div className='app'>
-      <Route exact path="/" render={() => (
-        <ListBooks
-          onChangeShelf={this.changeShelf.bind(this)}
-          books={this.state.books}
-          shelf={['currentlyReading', 'read', 'wantToRead']}/>
-
-        )}/>
-      <Route path="/search" render={() => (
-        <SearchBook
-            onChangeShelf={this.changeShelf.bind(this)}
-            books={this.state.books}
+  const changeShelf = async (book, shelf) => {
+    const updateResponse = await BooksAPI.update(book, shelf);
+    const getAllResponse = await BooksAPI.getAll();
+    setBooks(getAllResponse);
+    console.log(updateResponse);
+  };
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <Listbooks
+            onChangeShelf={changeShelf}
+            books={books}
+            shelf={arrShelf}
           />
-        )}/>
-      </div>
-    )
-  }
-}
-
+        }
+      />
+      <Route
+        path="/search"
+        element={<SearchBook onChangeShelf={changeShelf} books={books} />}
+      />
+    </Routes>
+  );
+};
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <BrowserRouter><App /></BrowserRouter>,
-   document.getElementById('root')
- )
+import "./index.css";
+import App from "./App";
+
+// I have changed this file because ReactDOM.render is no longer supported in React 18. Use createRoot instead. Until you switch to the new API, your app will behave as if it's running React 17. Learn more: https://reactjs.org/link/switch-to-createroot
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
